c2a9c91930721eed250829ca283b889146a13e29
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-02-12 01:19:59 +0600

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,11 +1,10 @@
 list=[]
 list= [3,4,2,8,9,12,43]
-
 print(list)
-max= list.index(max(list))
 
 #swap the minimal and the maximal element
 
+max= list.index(max(list))
 min =list.index(min(list))
 list[max], list[min] = list[min], list[max]
 
