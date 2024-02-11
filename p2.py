0e1c586a7c907faf4cad53b519436216197742de
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-02-12 01:58:34 +0600

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,9 +1,8 @@
-# Given list of numbers
-numbers = [1, 2, 3, 4, 5]
+
+n = [1, 2, 3, 4, 5]
+print(n)
 
 # Swap adjacent items in pairs
-for i in range(0, len(numbers) - 1, 2):
-    numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
-
-# Print the resulting list
-print(numbers)
+for i in range(0, len(n) - 1, 2):
+    n[i], n[i + 1] = n[i + 1], n[i]
+print(n)
