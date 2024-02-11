94809007c392b7cefa9c2a5d84c8784832199694
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-02-12 01:29:52 +0600

    Create p5.py

---
+++ p5.py
@@ -0,0 +1,12 @@
+# List of item names
+item_names = ["Pen", "BOok", "marKer"]
+
+# Case 01: Find out the length of each item and store in another list
+
+item_lengths = [len(item) for item in item_names]
+print(item_lengths)
+
+# Case 02: Access each item's name and convert the case of letters
+for item in item_names:
+    print(item.upper())  # Convert to uppercase
+    print(item.lower())  # Convert to lowercase
