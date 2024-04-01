4264b50d769fdf66cb4d33f7b757a265d45c740b
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-04-02 00:56:54 +0600

    Create 2.2.7.py

---
+++ 2.2.7.py
@@ -0,0 +1,7 @@
+def print_positive_numbers(lst):
+ for num in lst:
+ if num > 0:
+ print(num)
+my_list = [-5, -2, 4, -3, 10,62,45,3, -1, 0]
+print("Positive numbers in the list:")
+print_positive_numbers(my_list)
