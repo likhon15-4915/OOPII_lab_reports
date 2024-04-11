aabfbe14c86af7adf1a42d1b4de81cf0d38c7c2e
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-04-11 14:12:23 +0600

    Create 3.4.py

---
+++ 3.4.py
@@ -0,0 +1,7 @@
+def is_palindrome(s):
+ return s == s[::-1]
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+ print("The string is a palindrome.")
+else:
+ print("The string is not a palindrome.")
