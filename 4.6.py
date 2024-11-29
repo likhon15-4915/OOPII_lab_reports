45f00f89b161af0d399a2634ca2af9a6f909246a
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-11-29 11:14:43 +0600

    Create 4.6.py

---
+++ 4.6.py
@@ -0,0 +1,7 @@
+def concatenate_tuples(tuple1, tuple2):
+ new_tuple = tuple1 + tuple2
+ return new_tuple
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+new_tuple = concatenate_tuples(tuple1, tuple2)
+print("Concatenated Tuple:", new_tuple)
