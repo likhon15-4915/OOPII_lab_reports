118693b4a4a93d799059d2f088d37b7834959e3c
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-11-30 20:36:06 +0600

    Create 4.14.py

---
+++ 4.14.py
@@ -0,0 +1,4 @@
+set_A = set(input("Enter elements of set A separated by spaces: ").split()) 
+set_B = set(input("Enter elements of set B separated by spaces: ").split()) 
+intersection = set_A.intersection(set_B) 
+print("Common elements:", intersection) 
