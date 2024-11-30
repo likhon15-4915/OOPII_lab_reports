a734df3eb2b42e757c3f15560b53192469548bc6
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-11-30 20:37:30 +0600

    Create 4.17.py

---
+++ 4.17.py
@@ -0,0 +1,9 @@
+def count_key_value_pairs(dictionary): 
+num_pairs = len(dictionary) 
+return num_pairs 
+example_dict = { 
+"Name": "Alvi", 
+"Age": 34, 
+"Grade": "A" 
+} 
+print("Number of key-value pairs:", count_key_value_pairs(example_dict)) 
