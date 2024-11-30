7ea70331a7c9ddc9f96aea3aaa13579eca77a86a
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-11-30 20:37:06 +0600

    Create 4.16.py

---
+++ 4.16.py
@@ -0,0 +1,11 @@
+def print_dictionary_keys_and_values(dictionary): 
+for key in dictionary: 
+print("Key:", key) 
+print("Value:", dictionary[key]) 
+print() 
+example_dict = { 
+"Name": "Alvi", 
+"Age": 34, 
+"Grade": "A" 
+} 
+print_dictionary_keys_and_values(example_dict)
