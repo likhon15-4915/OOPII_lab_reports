d6f99689c45d33f18d66004ed5635aba8a4488d1
Md. Raisul Kabir Khan Likhon <114115052+likhon15-4915@users.noreply.github.com>
2024-11-30 20:38:31 +0600

    Create 4.19.py

---
+++ 4.19.py
@@ -0,0 +1,8 @@
+def merge_dictionaries(dict1, dict2): 
+merged_dict = dict1.copy() 
+merged_dict.update(dict2) 
+return merged_dict 
+dict1 = {'a': 1, 'b': 2} 
+dict2 = {'c': 3, 'd': 4} 
+merged_dict = merge_dictionaries(dict1, dict2) 
+print("Merged dictionary:", merged_dict) 
